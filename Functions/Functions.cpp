e21ed2024b477de9720ee36937a9278eeee88bcc
Bhaskar Maity <53265977+Bhaskar-maity@users.noreply.github.com>
2020-02-01 22:09:07 +0530

    Add files via upload

---
+++ Functions/Functions.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int max_of_four(int a, int b, int c, int d)
+{
+   int max=a;
+    if(b>max)
+     max=b;
+    if(c>max)
+     max=c;
+    if(d>max)
+     max=d;
+    return(max);
+    
+}
+
+
+int main() {
+    int a, b, c, d;
+    //scanf("%d %d %d %d", &a, &b, &c, &d);
+    cin>>a>>b>>c>>d;
+    int ans = max_of_four(a, b, c, d);
+    //printf("%d", ans);
+    cout<<ans;
+    
+    return 0;
+}
+
